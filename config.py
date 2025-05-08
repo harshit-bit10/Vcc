7078b794468ce40e8b784d2affd8f91103f8088b
harshit-bit10 <109902163+harshit-bit10@users.noreply.github.com>
2025-05-08 16:21:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config(object):
     # pyro client config
     API_ID    = os.environ.get("API_ID", "7603458")  # ⚠️ Required
     API_HASH  = os.environ.get("API_HASH", "910e420f1f74f40305a684a331dade35") # ⚠️ Required
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7439562089:AAGlEskgYwprQUgRE70Er74xttSWge9RLhI") # ⚠️ Required
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7439562089:AAHvAf5r7i0vzQC3E36W1pxI6P4QDQyc8TM") # ⚠️ Required
     FORCE_SUB = os.environ.get('FORCE_SUB', '-1002075434712') # ⚠️ Required
     AUTH_CHANNEL = int(FORCE_SUB) if FORCE_SUB and id_pattern.search(
     FORCE_SUB) else None
@@ -24,7 +24,7 @@ class Config(object):
 
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", True))
-    PORT = int(os.environ.get("PORT", "8098"))
+    PORT = int(os.environ.get("PORT", "8099"))
 
     caption = """
 **File Name**: {0}
